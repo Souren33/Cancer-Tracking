0d375dc58c6d81b1aed38c5364fbfd0b861a400e
Souren33 <prakashsouren@gmail.com>
2024-10-07 18:11:07 -0400

    API Progress

--- Cancer API.py
+++ Cancer API.py
@@ -1,4 +1,29 @@
-"hello world"
-"some changes"
-"atharva change"
-'new'
+"""
+File Cancer API
+Description: API for interacting with the dataset of focus
+"""
+
+import pandas as pd
+import altair as alt
+import seaborn as sns
+
+
+class CANAPI:
+
+    CAN = None
+
+    def load_CAN(self, filename):
+        self.CAN = pd.read_excel(filename)
+        print(self.pal)
+
+
+    def get_age(self):
+
+        CAN_areas = self.CAN.age
+
+    class G
+
+
+    #def get_locations(self):
+
+
