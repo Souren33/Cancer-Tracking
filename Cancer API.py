c298c0e22332cc0791b646dbbcfb2f07a384a598
Souren33 <prakashsouren@gmail.com>
2024-10-08 17:50:40 -0400

    updates

--- Cancer API.py
+++ Cancer API.py
@@ -7,23 +7,37 @@ import pandas as pd
 import altair as alt
 import seaborn as sns
 
+filename = r"C:\Users\Souren Prakash\OneDrive\Desktop\Cancer Tracking project\CTDC_Participants_download 2024-10-04 12-59-58.csv"
 
 class CANAPI:
 
-    CAN = None
+    can = None
 
-    def load_CAN(self, filename):
-        self.CAN = pd.read_excel(filename)
-        print(self.pal)
+    def load_can(self, filename):
+        self.can= pd.read_csv(filename, )
+       #print(self.can)
+        return self.can
 
+    def get_diagnosis(self):
 
-    def get_age(self):
-
-        CAN_areas = self.CAN.age
-
-    class G
-
-
-    #def get_locations(self):
+        can = self.can
+        can.Diagnosis = can.Diagnosis.str.lower()
+        diagnosis = can.Diagnosis.unique()
+        return diagnosis
 
 
+
+
+
+
+def main():
+
+    canapi = CANAPI()
+
+    canapi.load_can(filename)
+
+    print(canapi.get_diagnosis())
+
+
+if __name__ == '__main__':
+    main()
